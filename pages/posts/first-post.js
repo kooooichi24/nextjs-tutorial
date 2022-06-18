19b107f045d87602236c06dbf507de041d19212b
kooooichi24 <ex.kouichi1224@gmail.com>
2022-06-18 12:10:36 +0900

    Assets, Metadata, and CSS / Assets

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
+
+const YourComponent = () => (
+  <Image
+    src="/images/cat.jpeg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={288} // Desired size with correct aspect ratio
+    alt="Cat"
+  />
+);
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <YourComponent />
       <h2>
         <Link href="/">
           <a>Back to home</a>
